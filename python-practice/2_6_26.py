664ec8f8c931056eec8a9d8a08a50a596d2ef7f5
vivekkumaryadav7737-ctrl <263886611+vivekkumaryadav7737-ctrl@users.noreply.github.com>
2026-06-02 14:51:16 +0530

    Add files via upload

---
+++ python-practice/2_6_26.py
@@ -0,0 +1,60 @@
+#Built in functions
+# (1) print- to print text or numerical value
+"""
+print("Good Morning")
+print("Hello World")
+a=10
+print("a")
+print(a)
+print("Hello")
+hello=100
+#print(Hello) #error
+print(hello)
+print("Hello India",hello)
+
+print("Hello India","hello")
+
+a=10
+b=20
+c=a+b
+print(c)
+
+# (2) Input function- used to ask value from user, return a text or string value.
+a=input()
+b=input()
+c=a+b
+print("Addition:",c)  #contcatenate becuase input function bydef take string
+
+#(3) Integer function- Numerical but non-decimal value
+a=int(input("Enter a number:"))
+b=int(input("Enter b number:"))
+c=a+b
+print(c)
+
+#(4) float function- Numerical and decimal value
+a=float(input("Enter your first number:"))
+b=float(input("Enter you second number:"))
+c=a+b
+print("Addition:",c)
+
+#WAP to calculate gross salary of employee
+#HRA=20% of basic salary and DA=30% of basic salary
+
+bs=float(input('Enter your basi salary:'))
+hra=20*bs/100
+da=30*bs/100
+gs=bs+hra+da
+print('Your gross salary:',gs)
+
+#wap to calculate cube of a number taking no. as input from user
+a=int(input('Enter your number:'))
+cu=a*a*a
+print('Cube of number is:',cu)
+
+#wap to calculate area of circle
+r=float(input('Enter radius of circle:'))
+ar=3.14*r*r
+print('Area of Circle:',ar)
+"""
+
+
